2b2d0210fe777ebb5e362f1cb4fa67df4235a301
i.tihanovich <tikhanovich.89@gmail.com>
2018-06-12 13:50:16 +0300

    Стили

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html lang="en">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -14,8 +15,8 @@
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
-    <link rel="stylesheet" href="/css/bootstrap-datetimepicker.min.css?v=1.0.1" />
-    <link rel="stylesheet" href="/css/style.css?v=1.0.1" />
+    <link rel="stylesheet" href="css/bootstrap-datetimepicker.min.css?v=1.0.1" />
+    <link rel="stylesheet" href="css/style.css?v=1.0.1" />
 </head>
 <body id="app-layout">
     <nav class="navbar navbar-default navbar-static-top">
@@ -77,7 +78,7 @@
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-    <script src="/js/bootstrap-datetimepicker.min.js"></script>
-    <script src="/js/script.js?v=1.0.1"></script>
+    <script src="js/bootstrap-datetimepicker.min.js"></script>
+    <script src="js/script.js?v=1.0.1"></script>
 </body>
 </html>
